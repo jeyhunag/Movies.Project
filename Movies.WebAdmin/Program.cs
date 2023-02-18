ee0efe26a860d0b000f70c8e564593481d2c472a
jeyhunag <104305445+jeyhunag@users.noreply.github.com>
2023-02-18 17:43:52 +0400

    Update UI

--- Movies.WebAdmin/Program.cs
+++ Movies.WebAdmin/Program.cs
@@ -11,6 +11,7 @@ namespace Movies.WebAdmin
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+       
             builder.Services.AddDbContext<AppDbContext>(options =>
             {
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString"));
